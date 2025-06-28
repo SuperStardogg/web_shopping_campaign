a598d0148d76635e588f965337baccb78deb223e
SuperStardogg <theparry4@gmail.com>
2025-06-28 23:36:47 +0700

    feat: setup tailwindcss 💨 and install primeVue lib 🔌

--- apps/client/src/main.js
+++ apps/client/src/main.js
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 import routes from '~pages'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,5 +14,5 @@ const router = createRouter({
 
 const app = createApp(App)
 app.use(router);
+app.use(PrimeVue);
 app.mount('#app')
-
--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
-
-const pathAlias = {
-  '@': path.resolve(__dirname, './src'),
-  '#': path.resolve(__dirname, './types'),
-  '@root': path.resolve(__dirname, './'),
-}
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: pathAlias,
-  },
-  plugins: [vue(), Pages({
-    dirs: 'src/pages',
-  })],
+  plugins: [
+    vue(),
+    Pages({
+      dirs: 'src/pages',
+    }),
+  ],
 })
