cccf83590713966a9d7a9b87f3b45db584c16bf3
SuperStardogg <theparry4@gmail.com>
2025-06-28 23:38:46 +0700

    feat: UI card component

---
+++ apps/client/src/components/ui/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bg-light-50 flex flex-col gap-2 p-4 text-left rounded-lg card--shadow"
+  >
+    <div class="text-lg">
+      <slot name="title" />
+    </div>
+    <div>
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.card--shadow {
+  box-shadow: 0px 3px 12px 0px rgba(0, 0, 0, 0.15);
+  &:hover {
+    box-shadow: 5px 4px 16px 3px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
